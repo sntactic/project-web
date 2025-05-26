45f5913a660b9081b440038449b44fc5886fcc93
sntactic <khadim.ahmad.mbaye@gmail.com>
2025-05-26 13:36:09 +0000

    maj landing-page

--- QuizzCards/src/app/app.component.ts
+++ QuizzCards/src/app/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import { HeaderComponent } from './header/header.component'
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet,NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,22 @@ import { RouterOutlet } from '@angular/router';
     HeaderComponent,
     TopBarComponent,
     SideBarComponent,
-    RouterOutlet
+    RouterOutlet,
+    CommonModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent //implements AfterViewInit
 {
+  currentRoute!: string;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+  
 }
